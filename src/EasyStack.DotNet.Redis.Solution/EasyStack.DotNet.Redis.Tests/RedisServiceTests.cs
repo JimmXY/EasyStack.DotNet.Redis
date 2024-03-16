d8e5f3a8ca34981d0ee22bf03b3a261a019ae041
JimmXY <hariharana87@gmail.com>
2024-03-16 15:24:23 -0500

    Starter definitions

---
+++ src/EasyStack.DotNet.Redis.Solution/EasyStack.DotNet.Redis.Tests/RedisServiceTests.cs
@@ -0,0 +1,29 @@
+using EasyStack.DotNet.Redis.Abstractions;
+
+namespace EasyStack.DotNet.Redis.Tests
+{
+    [TestClass]
+    public class RedisServiceTests
+    {
+        private IRedisService _testRedisService;
+
+        // store and retreive primitive types from redis
+        // store and retreive simple types from redis
+        // store and retreive complex types from redis
+        // store and retreive complex types and their properties from redis       
+
+
+        [TestMethod]
+        public void GivenAnItemToStoreInRedisWithoutExpiration_WhenRetreivedUsingGet_ReturnsTheExpectedItem()
+        {
+            // Arrange
+
+
+            // Act
+
+            // Assert
+
+
+        }
+    }
+}
--- src/EasyStack.DotNet.Redis.Solution/EasyStack.DotNet.Redis/Abstractions/IRedisService.cs
+++ src/EasyStack.DotNet.Redis.Solution/EasyStack.DotNet.Redis/Abstractions/IRedisService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,43 @@ namespace EasyStack.DotNet.Redis.Abstractions
 {
     public interface IRedisService
     {
-        
+        /// <summary>
+        /// Gets an item from the cache
+        /// </summary>
+        /// <typeparam name="T">The type of item to get</typeparam>
+        /// <param name="key">The key of item to get from</param>
+        /// <returns>The instance of the item as stored in Redis</returns>
+        T GetItem <T>(string key);
+
+        T GetItemProperty<T>(string key, MemberExpression propertyExpression);
+
+        /// <summary>
+        /// Stores an item in the cache
+        /// </summary>
+        /// <typeparam name="T">The type of the item to store</typeparam>
+        /// <param name="key">The key to store the item under in Redis</param>
+        /// <param name="value">The value to be stored in Redis under the given Key</param>
+        void SetItem<T>(string key, T value);
+
+        void SetItemProperty<T>(string key, MemberExpression propertyExpression, T value);
+
+        /// <summary>
+        /// Stores an item in the cache
+        /// </summary>
+        /// <typeparam name="T">The type of the item to store</typeparam>
+        /// <param name="key">The key to store the item under in Redis</param>
+        /// <param name="value">The value to be stored in Redis under the given Key</param>
+        /// <param name="expiration">The time interval in which the stored cache item will be deleted automatically by Redis</param>
+        void SetItem<T>(string key, T value, TimeSpan expiration);
+
+        /// <summary>
+        /// Removes an item from the cache
+        /// </summary>
+        /// <param name="key">The key to be removed from Redis</param>
+        void RemoveItem(string key);
+
+
+
+
     }
 }
